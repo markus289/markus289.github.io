8f40eb95d8b7e3215bc2bae2ea5d2671a98dd622
Markus Rothe <markus.rothe@rite.cc>
2018-02-26 11:28:46 +0100

    Add deploy scipt, exclude it from being deployed

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+edo()
+{
+    CYAN='\033[0;36m'
+    NC='\033[0m'
+    echo -e " ${CYAN}*${NC} $@" 1>&2
+    "$@"
+}
+
+edo bundle install
+edo bundle exec jekyll build
+edo find _site -type d -exec chmod 755 {} \;
+edo find _site -type f -exec chmod 644 {} \;
+edo rsync -av --partial --progress --delete _site/ doxy.rite.cc:/var/www/markus8191.de
