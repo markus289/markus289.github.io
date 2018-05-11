5832266ecc00a874c5d9840ba22e750f02f791c8
Markus Rothe <markus.rothe@rite.cc>
2018-05-11 07:50:04 +0000

    deploy: error on failures

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,18 @@ edo()
     "$@"
 }
 
-edo bundle install
-edo bundle exec jekyll build
-edo find _site -type d -exec chmod 755 {} \;
-edo find _site -type f -exec chmod 644 {} \;
-edo rsync -av --partial --progress --delete _site/ doxy.markus8191.de:/var/www/markus8191.de
+eerror()
+{
+    RED='\033[0;31m'
+    NC='\033[0m'
+    echo -e " ${RED}**ERROR**${NC} $@"
+    exit 1
+}
+
+[[ -x $(which bundle) ]] || eerror "'bundle' not in \$PATH"
+edo bundle install || eerror "'bundle install' failed"
+edo bundle exec jekyll build || eerror "'jekyll build' failed"
+edo find _site -type d -exec chmod 755 {} \; || eerror "chmod directories failed"
+edo find _site -type f -exec chmod 644 {} \; || eerror "chmod files failed"
+[[ -x $(which rsync) ]] || eerorr "'rsync' not in \$PATH"
+edo rsync -av --partial --progress --delete _site/ doxy.markus8191.de:/var/www/markus8191.de || eerror "rsync failed"
